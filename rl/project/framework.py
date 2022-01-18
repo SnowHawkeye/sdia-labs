92f5d697a3d0992c39e343927f22d27f0b44d3a6
SnowHawkeye <hadriensalem@gmail.com>
2022-01-18 14:53:47 +0100

    RL - Project (finished implementing ETC agent)

--- rl/project/framework.py
+++ rl/project/framework.py
@@ -38,8 +38,17 @@ class Arm():
         else : return results
 
 class Environment():
+    """An environment contains a set of arms. It lets a user pull any arm and exposes the history of rewards observed when doing so.
+    """
     def __init__(self, arms):
+        """Creates an environment with the given list of arms.
 
+        Args:
+            arms (list(Arm)): The list of arms to initialize the environment with. They must all have the same variance.
+
+        Raises:
+            ValueError: If all arms do not have the same variance.
+        """
         # Ensure that all arms have the same known variance
         for a in arms :
             if a.std != arms[0].std : raise ValueError("All arms within the environment must have the same variance.")
@@ -47,22 +56,17 @@ class Environment():
         self.arms = arms
         self.std = self.arms[0].std
         self.reward_history = [ [] for _ in range(len(self.arms)) ] # Contains the observed rewards
-        self.best_arm_history = [] # Contains the list of the best arm that could have been chosen each time a pull happened
-        self.best_potential_reward_history = [] # Contains the best possible reward that could have been obtained each time a pull happened
         
     def pull_arm(self, arm_number):
-        # We pull every arm in order to know the best one, but only record the one that is requested
-        
-        rewards = np.zeros(len(self.arms))
-        for i in range(len(self.arms)):
-            rewards[i] = self.arms[i].pull()
-        
-        # Update the best potential arms
-        self.best_arm_history.append(np.argmax(rewards))
-        self.best_potential_reward_history.append(np.max(rewards))
-        
-        # Compute the reward for the requested arm
-        reward = rewards[arm_number]
+        """Pull the requested arm.
+
+        Args:
+            arm_number (int): The arm to pull.
+
+        Returns:
+            float: The reward obtained by pulling the arm.
+        """
+        reward = self.arms[arm_number].pull()
         self.reward_history[arm_number].append(reward)
         return reward
 
@@ -70,23 +74,29 @@ class Environment():
         """Resets the environment's history.
         """
         self.reward_history = [ [] for _ in range(len(self.arms)) ]
-        self.best_arm_history = []
-        self.best_potential_reward_history = []
     
 ################# AGENTS #################
 
 class Agent(metaclass=abc.ABCMeta):
+    """Abstract class representing an arm-pulling agent.
+    """
     @abc.abstractmethod
-    def play(self, steps, confidence, std, env):
-      pass
+    def play(self, steps, confidence, env):
+        """The agent plays in the given environment and returns the relevant results.
+
+        Args:
+            steps (int): The number of steps to play.
+            confidence (float): The confidence level (between 0 and 1).
+            env (Environment): The environment to play in.
+            
+        Returns:
+            PlayResults: The results obtained at the end of the session.
+        """
+        pass
   
 class ETC_Agent(Agent):
-    # works for two arms
-    # implemented as described in the article
-    
-    def __init__(self):
-        self.is_exploring = True # if false, means that the agent is exploiting
-        self.last_choice = 0 # choices are 0 or 1 for this agent
+    """An arm-pulling agent using the Explore-Then-Commit strategy.
+    """
     
     def play(self, steps, confidence, env):
         if len(env.arms) != 2 : raise ValueError("This agent requires exactly two arms to play.")
@@ -94,11 +104,11 @@ class ETC_Agent(Agent):
         
         # Initialization
         env.reset_history()
-        env.pull_arm(0)
-        env.pull_arm(1)
         n = 2
-        chosen_arms = []
+        chosen_arms = [0, 1]
         rewards_obtained = []
+        rewards_obtained.append(env.pull_arm(0))
+        rewards_obtained.append(env.pull_arm(1))
         
         # Exploration loop
         while abs(np.mean(env.reward_history[0]) - np.mean(env.reward_history[1])) < np.sqrt((4*(env.std**2)/n)*np.log((np.log(n))**2/confidence)) and n < steps:
@@ -107,6 +117,8 @@ class ETC_Agent(Agent):
             chosen_arms.append(0)
             chosen_arms.append(1)
             n += 2
+        
+        decision_time = n # The desired confidence has been reached
         
         # Exploitation loop
         while n < steps:
@@ -121,10 +133,29 @@ class ETC_Agent(Agent):
                 rewards_obtained.append(env.pull_arm(1))
                 chosen_arms.append(1)
                 
-        return chosen_arms, rewards_obtained
+        return PlayResults(chosen_arms, rewards_obtained, decision_time, env)
+    
+################# RESULTS #################
+    
+class PlayResults():
+    def __init__(self, chosen_arms, rewards_obtained, decision_time, env):
+        self.chosen_arms = np.array(chosen_arms)
+        self.rewards_obtained = np.array(rewards_obtained)
+        self.env = env
+        
+        self.regret = self.__compute_regret()
+        self.decision_time = decision_time
+        self.decision_regret = self.__compute_regret(until=self.decision_time)
+        
+    def __compute_regret(self, until = None):
+        T = len(self.chosen_arms)
+        if until == None: until = T
+        
+        max_reward = np.max([arm.mean for arm in self.env.arms])
+        cumulative_rewards = np.sum(self.rewards_obtained)
+        return T * max_reward - cumulative_rewards
 
 #################################### FUNCTIONS ####################################
-    
     
 def gaussian_sampling(mean, std):
     """Samples a random number from a gaussian distribution.
