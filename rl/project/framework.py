3e613348c8a31d872504829d62ca884c2ec43c86
SnowHawkeye <hadriensalem@gmail.com>
2022-01-18 16:47:31 +0100

    RL - Project implementation (UCB alpha agent implemented)

--- rl/project/framework.py
+++ rl/project/framework.py
@@ -95,7 +95,7 @@ class Agent(metaclass=abc.ABCMeta):
         pass
   
 class ETC_Agent(Agent):
-    """An arm-pulling agent using the Explore-Then-Commit strategy.
+    """An arm-pulling agent using the Explore-Then-Commit strategy. Implemented for 2 arms.
     """
     
     def play(self, steps, confidence, env):
@@ -111,7 +111,7 @@ class ETC_Agent(Agent):
         rewards_obtained.append(env.pull_arm(1))
         
         # Exploration loop
-        while abs(np.mean(env.reward_history[0]) - np.mean(env.reward_history[1])) < np.sqrt((4*(env.std**2)/n)*np.log((np.log(n))**2/confidence)) and n < steps:
+        while abs(np.mean(env.reward_history[0]) - np.mean(env.reward_history[1])) < np.sqrt((4*(env.std**2)/n)*np.log((np.log(n))**2/confidence)) and n <= steps:
             rewards_obtained.append(env.pull_arm(0))
             rewards_obtained.append(env.pull_arm(1))
             chosen_arms.append(0)
@@ -135,6 +135,88 @@ class ETC_Agent(Agent):
                 
         return PlayResults(chosen_arms, rewards_obtained, decision_time, env)
     
+
+class UCBAlphaAgent(Agent):
+    """An arm-pulling agent using the UCB_alpha strategy. Implemented for 2 arms, but could be generalized.
+    """
+    
+    def __init__(self, alpha):
+        self.alpha = alpha
+    
+    def play(self, steps, confidence, env):
+        if len(env.arms) != 2 : raise ValueError("This agent requires exactly two arms to play.")
+        if steps < 3 : raise ValueError("The number of steps must be at least 3.")
+        
+        # Initialization
+        env.reset_history()
+        n = 2
+        n_pulled_0 = 2
+        n_pulled_1 = 2
+        best_arm = None
+        
+        # We have to pull each arm twice initially to avoid a log(0) in the epsilon computation later on
+        chosen_arms = [0, 1, 0, 1]
+        rewards_obtained = []
+        
+        rewards_obtained.append(env.pull_arm(0))
+        rewards_obtained.append(env.pull_arm(1))
+        
+        rewards_obtained.append(env.pull_arm(0))
+        rewards_obtained.append(env.pull_arm(1))
+        
+        while True:
+            n += 1
+            
+            # Compute scores
+            epsilon0 = self.__compute_epsilon(n_pulled_0, env.std, confidence) 
+            epsilon1 = self.__compute_epsilon(n_pulled_1, env.std, confidence) 
+            
+            r0 = np.mean(env.reward_history[0])
+            r1 = np.mean(env.reward_history[1])
+            
+            score0 = r0 + self.alpha * epsilon0
+            score1 = r1 + self.alpha * epsilon1
+            
+            # Pick an action
+            if score0 > score1:
+                rewards_obtained.append(env.pull_arm(0))
+                chosen_arms.append(0)
+                n_pulled_0 += 1
+                
+                # Check if the best arm was found or if the maximum number of steps was reached
+                if r0 - epsilon0 > r1 + epsilon1 or n > steps:
+                    best_arm = 0
+                    break
+
+            else :
+                rewards_obtained.append(env.pull_arm(1))
+                chosen_arms.append(1)
+                n_pulled_1 += 1
+                
+                # Check if the best arm was found or if the maximum number of steps was reached
+                if r1 - epsilon1 > r0 + epsilon0 or n > steps:
+                    best_arm = 1
+                    break
+                
+        decision_time = n # The desired confidence has been reached
+        
+        # The best arm was found, we now switch to pure exploitation
+        # n_pulled_X are not updated from now on because they are no longer needed
+        while n < steps:
+            n += 1
+            chosen_arms.append(best_arm)
+            rewards_obtained.append(env.pull_arm(best_arm))
+
+        return PlayResults(chosen_arms, rewards_obtained, decision_time, env)
+    
+    def __compute_epsilon(self, n_pulled, sigma, delta):
+        """Computes the epsilon coefficient introduced by the article.
+        """
+        coef = (2*(sigma**2))/n_pulled
+        in_log = 3*(np.log(n_pulled)**2) / delta
+        
+        return np.sqrt(coef * np.log(in_log))
+    
 ################# RESULTS #################
     
 class PlayResults():
@@ -152,11 +234,11 @@ class PlayResults():
         if until == None: until = T
         
         max_reward = np.max([arm.mean for arm in self.env.arms])
-        cumulative_rewards = np.sum(self.rewards_obtained)
-        return T * max_reward - cumulative_rewards
+        cumulative_rewards = np.sum(self.rewards_obtained[:until])
+        return until * max_reward - cumulative_rewards
 
-#################################### FUNCTIONS ####################################
-    
+#################################### UTILITYFUNCTIONS ####################################
+
 def gaussian_sampling(mean, std):
     """Samples a random number from a gaussian distribution.
 
